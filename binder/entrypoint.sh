c46497afad3b00858931e64e67570a94602b72c5
hawkina <hawkin@informatik.uni-bremen.de>
2024-03-10 20:14:56 +0100

    trying to fix entry point

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -7,7 +7,7 @@ roslaunch --wait rvizweb rvizweb.launch &
 roslaunch --wait roslaunch cram_projection_demos everything.launch retail:=true donbot:=true upload_robot:=true tf2_buffer:=true &
 
 
-jupyter lab workspaces import ${HOME}/.binder/workspace.json
+jupyter lab workspaces import ${HOME}/.binder/rviz_config/retail_donbot.launch.json
 
 # Use xvfb virtual display when there is no display connected.
 if [ -n "$DISPLAY" ]; then
