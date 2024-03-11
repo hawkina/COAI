d2b0d7148c43d897fa007de6690ec1ba6d0c48d4
hawkina <hawkin@informatik.uni-bremen.de>
2024-03-11 16:40:21 +0100

    visualizing rviz and bullet

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,9 +2,9 @@
 
 source ${HOME}/workspace/ros/devel/setup.bash
 
-roscore &
-roslaunch --wait rvizweb rvizweb.launch &
-roslaunch --wait cram_projection_demos retail_donbot.launch &
+#roscore &
+#roslaunch --wait rvizweb rvizweb.launch &
+#roslaunch --wait cram_projection_demos retail_donbot.launch &
 
 
 jupyter lab workspaces import ${HOME}/.binder/rviz_config/retail_donbot.launch.json
