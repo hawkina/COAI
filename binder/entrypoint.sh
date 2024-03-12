7b727b80a368443626c3127e61e1167044c4f890
HaSu <hawkin@uni-bremen.de>
2024-03-12 13:27:51 +0100

    remove launch from entrypoint

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,8 +3,8 @@
 source ${HOME}/workspace/ros/devel/setup.bash
 
 roscore &
-roslaunch --wait rvizweb rvizweb.launch
-roslaunch --wait cram_projection_demos retail_donbot.launch &
+roslaunch --wait rvizweb rvizweb.launch &
+#roslaunch --wait cram_projection_demos retail_donbot.launch &
 
 
 jupyter lab workspaces import ${HOME}/.binder/rviz_config/retail_donbot.launch.json
