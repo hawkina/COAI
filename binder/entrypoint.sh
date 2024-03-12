f9f268b5ad27c92a77aa70b245d3de2f38dabf91
HaSu <hawkin@uni-bremen.de>
2024-03-12 14:58:43 +0100

    adapt entrypoint and update_scene

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,15 +3,15 @@
 source ${HOME}/workspace/ros/devel/setup.bash
 
 roscore &
-#roslaunch --wait rvizweb rvizweb.launch &
+roslaunch --wait rvizweb rvizweb.launch &
 #roslaunch --wait cram_projection_demos retail_donbot.launch &
 
 
 jupyter lab workspaces import ${HOME}/.binder/rviz_config/retail_donbot.launch.json
 
 # Use xvfb virtual display when there is no display connected.
-if [ -n "$DISPLAY" ]; then
-    exec "$@"
-else
+#if [ -n "$DISPLAY" ]; then
+#    exec "$@"
+#else
     xvfb-run exec "$@"
-fi
+#fi
