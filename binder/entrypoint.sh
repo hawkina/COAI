4b37cb99b7b52dac1876c4bdb957ee2b62efe19a
HaSu <hawkin@uni-bremen.de>
2024-03-12 12:50:40 +0100

    disable xpra

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,7 @@ source ${HOME}/workspace/ros/devel/setup.bash
 
 roscore &
 roslaunch --wait rvizweb rvizweb.launch
-#roslaunch --wait cram_projection_demos retail_donbot.launch &
+roslaunch --wait cram_projection_demos retail_donbot.launch &
 
 
 jupyter lab workspaces import ${HOME}/.binder/rviz_config/retail_donbot.launch.json
